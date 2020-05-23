54823ab877fedf56304e21daca6090575d3a211b
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-05-23 14:24:13 +0100

    Check for timer and service before install

--- install.sh
+++ install.sh
@@ -2,12 +2,26 @@
 
 install-service-files() {
 	local apps="${1:?must provide apps}"
-	for app in $apps; do sudo mv -v "./$app/$app.service" /etc/systemd/system/; done
+	for app in $apps; do
+		local appService="./$app/$app.service"
+		if [[ ! -f "$appService" ]]; then
+			echo "Skipping service install: No service for app:$app."
+			continue
+		fi
+		sudo mv -v "$appService" /etc/systemd/system/;
+	done
 }
 
 install-timer-files() {
 	local apps="${1:?must provide apps}"
-	for app in $apps; do sudo mv -v "./$app/$app.timer" /etc/systemd/system/; done
+	for app in $apps; do
+		local appTimer="./$app/$app.timer"
+		if [[ ! -f "$appTimer" ]]; then
+			echo "Skipping timer install: No timer for app:$app."
+			continue
+		fi
+		sudo mv -v "$appTimer" /etc/systemd/system/;
+	done
 }
 
 enable-services() {
@@ -21,4 +35,3 @@ install-service-files "$apps"
 install-timer-files "$apps"
 sudo systemctl daemon-reload
 enable-services "$apps"
-enable-timers "$apps"
